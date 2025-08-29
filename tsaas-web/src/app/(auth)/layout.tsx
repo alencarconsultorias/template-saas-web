aeb617d127f67764e999b442d45977fd260df3fa
Lucas Lessa <lessalucas1102@gmail.com>
2025-08-29 08:48:32 -0300

    fix(TSAAS-113): darkmode-form

--- tsaas-web/src/app/(auth)/layout.tsx
+++ tsaas-web/src/app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-secondary-dark flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex items-center justify-center p-4">
       {children}
     </div>
   );
-} 
+}
